f523bdd6c7c2e65fac26cb7dbc7ede47d3d364b2
KsushaSeliv <KsenijaSeliv@yandex.ru>
2017-12-15 18:26:48 +0300

    Update rot13.py

--- rot13.py
+++ rot13.py
@@ -1,10 +1,9 @@
 def rot_13(text):
-    def rot(englishlang):
-        i='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.find(englishlang)
-        if i!=-1:
-            return 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'[i]
-        else:
-            return englishlang
-
-    return "".join(map(rot, text))
+ def rot(englishlang):
+  i='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.find(englishlang)
+  if i!=-1:
+   return 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'[i]
+  else:
+   return englishlang
+  return "".join(map(rot, text))
 print(rot_13('noenpnqnoen'))  #результат: abracadabra
